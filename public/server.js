ff0f54547a24c88c3a34852c8e9879fb5b3c939a
Thomas G Musler <thomas.g.musler@gmail.com>
2021-04-19 14:59:26 -0400

    fixed routes to workinng status

--- public/server.js
+++ public/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 
 const app = express();
 
@@ -12,22 +12,15 @@ app.use(express.json());
 
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 })
-
-
-app.route("/api/notes")
-    .get(function (req, res) {
-    res.json(database);
-})
-
 
 
 app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
+    console.log(`Aye we be sailing towards PORT: ${PORT}`);
   });
   
